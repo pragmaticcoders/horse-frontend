9fb8e9d2355cf18fe666cfdc993cc230b28df680
Marcin Biernat <mb@marcinbiernat.pl>
2016-12-01 14:18:01 +0100

    add material ui

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,18 @@
 import styles from './index.scss';
 import React from 'react';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
-        <p>Enjoy!</p>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <h1>HORSe</h1>
+            </MuiThemeProvider>
+        )
+    }
 }
